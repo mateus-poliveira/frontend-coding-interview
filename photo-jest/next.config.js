415278775f64010a5fdbbe16be39558def4931d0
Mateus Oliveira <mrxmath@hotmail.com>
2025-08-02 19:39:31 -0300

    chore: add retirect to photos

--- photo-jest/next.config.js
+++ photo-jest/next.config.js
@@ -1,4 +1,13 @@
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/photos",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
